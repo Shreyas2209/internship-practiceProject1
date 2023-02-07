432bb67d1420fa01a77da2369d767eea9884fe11
R.Shreyas <rshreyas711@gmail.com>
2023-02-07 17:07:17 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import DesktopHeader from "./Components/DesktopHeader";
+import MobileHeader from "./Components/MobileHeader";
+
+const checkIsMobileView = () => {
+  if (window.innerWidth <= 500) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 function App() {
+  const [isMobileView, setIsMobileView] = useState(checkIsMobileView());
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setIsMobileView(checkIsMobileView());
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isMobileView && <MobileHeader></MobileHeader>}
+      {!isMobileView && <DesktopHeader></DesktopHeader>}
+    </>
   );
 }
 
